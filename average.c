1e9dc69c70fa740e4f6c0f72dc3e9e75008bf9ba
deepakkumar1997 <pandeydeepak.kumar1997@gmail.com>
2019-08-28 15:06:28 -0400

    omg

--- average.c
+++ average.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
-
-
 void main()
 {
-	int a,c=0,i,n;
+	int c=0,i,m;
+	float a,n;
 	printf("Average of How many No.s do U want : ");
-	scanf("%d",n);
-	printf("Enter %d Numbers : ",n);
+	scanf("%f",&n);
+	printf("Enter %.0f Numbers : ",n);
 	for(i=0;i<n;i++)
 	{
-		scanf("%d",i);
-		c=c+i;
+		scanf("%d",&m);
+		c=c+m;
 	}
 	a=c/n;
-	printf("Average = ",a);
+	printf("Average = %.2f",a);
 }
 	
