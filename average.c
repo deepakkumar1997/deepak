146db7bb6834fa5c90d64aa85b2581a086f77dfe
deepakkumar1997 <pandeydeepak.kumar1997@gmail.com>
2019-08-28 14:20:10 -0400

    file

---
+++ average.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a,c=0,i,n;
+	printf("Average of How many No.s do U want : ");
+	scanf("%d",n);
+	printf("Enter %d Numbers : ",n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",i);
+		c=c+i;
+	}
+	a=c/n;
+	printf("Average = ",a);
+}
+	
